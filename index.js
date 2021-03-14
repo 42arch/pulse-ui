64c125bd726a7a1b3ede5d9872d90cb0fdfbfe8a
mainissues <rend42@163.com>
2021-03-15 00:07:43 +0800

    add loading component

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ import Button from './packages/button/index.js';
 import Switch from './packages/switch/index.js';
 import Row from './packages/row/index.js';
 import Col from './packages/col/index.js';
+import Loading from './packages/loading/index.js';
 
 
 const components = [
   Button,
   Switch,
   Row,
-  Col
-];
+  Col,
+  Loading
+]
 
 const install = function (Vue, opt = {}) {
   components.forEach(component => {
-    Vue.component(component.name, component);
-  });
-};
+    Vue.component(component.name, component)
+  })
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+  install(window.Vue)
 }
 
 export default {
@@ -26,5 +28,6 @@ export default {
   Button,
   Switch,
   Row,
-  Col
-};
+  Col,
+  Loading
+}
---
+++ packages/loading/src/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="classObj">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlLoading',
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classObj() {
+      return [
+        'pulse-loading',
+        { 'active': this.active }
+      ]
+    }
+  }
+}
+</script>
