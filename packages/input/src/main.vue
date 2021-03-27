4ec70d1329edb6c10e610b73f8d949e2927c34d7
mainissues <rend42@163.com>
2021-03-28 00:34:16 +0800

    input default style

--- packages/input/src/main.vue
+++ packages/input/src/main.vue
@@ -1,14 +1,47 @@
 <template>
   <div :class="classObj">
-    <div class="label-left">
+    <div class="prefix" v-if="prefix" v-html="prefix">
 
     </div>
+    <div class="suffix">
+
+    </div>
+    <textarea v-if="type === 'textarea'" :value="value" @input="updateValue($event.target.value)"></textarea>
+    <input v-else-if="type === 'text'" :type="type" :value="value" @input="updateValue($event.target.value)">
+    <slot></slot>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PlInput'
+  name: 'PlInput',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    classObj() {
+      return [
+        'pulse-input',
+        { 'disabled': this.disabled }
+      ]
+    }
+  },
+  methods: {
+    updateValue(val) {
+      this.$emit('input', val)
+    }
+  }
 }
 </script>
